46bd7d0844775cb16c658d441f4a097cab0b4bf0
m-falkowska@wp.pl <m-falkowska@wp.pl>
2025-11-22 12:05:36 +0100

    pierwsze kontrolery

---
+++ SolutionReact/SolutionReact.Server/Controllers/DestinationController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SolutionReact.Server.Models;
+
+namespace SolutionReact.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DestinationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Destination
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Destination>>> GetDestination()
+        {
+            return await _context.Destination.ToListAsync();
+        }
+
+        // GET: api/Destination/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Destination>> GetDestination(int id)
+        {
+            var destination = await _context.Destination.FindAsync(id);
+
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            return destination;
+        }
+
+        // PUT: api/Destination/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDestination(int id, Destination destination)
+        {
+            if (id != destination.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(destination).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DestinationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Destination
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Destination>> PostDestination(Destination destination)
+        {
+            _context.Destination.Add(destination);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDestination", new { id = destination.Id }, destination);
+        }
+
+        // DELETE: api/Destination/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDestination(int id)
+        {
+            var destination = await _context.Destination.FindAsync(id);
+            if (destination == null)
+            {
+                return NotFound();
+            }
+
+            _context.Destination.Remove(destination);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DestinationExists(int id)
+        {
+            return _context.Destination.Any(e => e.Id == id);
+        }
+    }
+}
